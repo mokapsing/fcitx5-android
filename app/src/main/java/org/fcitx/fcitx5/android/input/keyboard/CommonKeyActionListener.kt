3833512c62e9534d531499a55f5ff847c06d7ec7
Rocka <i@rocka.me>
2024-08-14 23:24:25 +0800

    Fix first backspace swipe after initialization

--- app/src/main/java/org/fcitx/fcitx5/android/input/keyboard/CommonKeyActionListener.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/input/keyboard/CommonKeyActionListener.kt
@@ -163,7 +163,7 @@ class CommonKeyActionListener :
                         Stopped -> {
                             backspaceSwipeState = if (
                                 preeditState.isEmpty &&
-                                horizontalCandidate.adapter.total == 0
+                                horizontalCandidate.adapter.total <= 0 // total is -1 on initialization
                             ) {
                                 service.applySelectionOffset(action.start, action.end)
                                 Selection
